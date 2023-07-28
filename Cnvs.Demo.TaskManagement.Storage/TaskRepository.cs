366c7272e2a971729f9d581e773c577174979571
Anton Zhilin <anton@innovine.nl>
2023-07-28 13:53:05 +0200

    Add logging to Task and User Repositories.

--- Cnvs.Demo.TaskManagement.Storage/TaskRepository.cs
+++ Cnvs.Demo.TaskManagement.Storage/TaskRepository.cs
@@ -1,36 +1,50 @@
+using Microsoft.Extensions.Logging;
 using Task = Cnvs.Demo.TaskManagement.Domain.Task;
 
 namespace Cnvs.Demo.TaskManagement.Storage;
 
 public class TaskRepository : ITaskRepository
 {
+    private readonly ILogger<TaskRepository> _logger;
+
+    public TaskRepository(ILogger<TaskRepository> logger)
+    {
+        _logger = logger;
+    }
+
     public async Task<Result<string>> DeleteTaskAsync(Guid taskId)
     {
+        _logger.LogInformation("Deleting task {TaskId}", taskId);
         throw new NotImplementedException();
     }
 
     public async Task<Result<Task>> AddTask(Task task)
     {
+        _logger.LogInformation("Adding task {TaskId}", task.Id);
         throw new NotImplementedException();
     }
 
     public async Task<Result<Task>> GetTaskAsync(Guid taskId)
     {
+        _logger.LogInformation("Getting task {TaskId}", taskId);
         throw new NotImplementedException();
     }
 
     public async Task<Result<IEnumerable<Task>>> GetTasksAsync()
     {
+        _logger.LogInformation("Getting tasks");
         throw new NotImplementedException();
     }
 
     public async Task<Result<Task>> UpdateTaskAsync(Task task)
     {
+        _logger.LogInformation("Updating task {TaskId}", task.Id);
         throw new NotImplementedException();
     }
 
     public async Task<Result<IEnumerable<Task>>> GetUserTasksAsync(string userName)
     {
+        _logger.LogInformation("Getting tasks for user {UserName}", userName);
         throw new NotImplementedException();
     }
 }
--- Cnvs.Demo.TaskManagement.Storage/UserRepository.cs
+++ Cnvs.Demo.TaskManagement.Storage/UserRepository.cs
@@ -1,31 +1,89 @@
 ﻿using Cnvs.Demo.TaskManagement.Domain;
+using Microsoft.Extensions.Logging;
 
 namespace Cnvs.Demo.TaskManagement.Storage;
 
 public class UserRepository : IUserRepository
 {
+    private readonly ILogger<UserRepository> _logger;
+
+    public UserRepository(ILogger<UserRepository> logger)
+    {
+        _logger = logger;
+    }
+
     public async Task<Result<User>> GetRandomUserAsync()
     {
-        throw new NotImplementedException();
+        try
+        {
+            _logger.LogInformation("Attempting to retrieve random user");
+            throw new NotImplementedException();
+            // _logger.LogInformation("Successfully retrieved random user: {userId}", user.Id);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error occurred while getting the random user");
+            throw;
+        }
     }
 
     public async Task<Result<User>> AddUser(User user)
     {
-        throw new NotImplementedException();
+        try
+        {
+            _logger.LogInformation("Adding new user: {userName}", user.Name);
+            throw new NotImplementedException();
+            // _logger.LogInformation("Successfully added user: {userName}", user.Name);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error occurred while adding user: {userName}", user.Name);
+            throw;
+        }
     }
 
     public async Task<Result<User>> GetUserAsync(string userName)
     {
-        throw new NotImplementedException();
+        try
+        {
+            _logger.LogInformation("Searching for user: {userName}", userName);
+            throw new NotImplementedException();
+            // _logger.LogInformation("Found user: {userName}", userName);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error occurred while retrieving user: {userName}", userName);
+            throw;
+        }
     }
 
     public async Task<Result<IEnumerable<User>>> GetUsersAsync()
     {
-        throw new NotImplementedException();
+        try
+        {
+            _logger.LogInformation("Getting all users");
+            throw new NotImplementedException();
+            // _logger.LogInformation("Successfully retrieved all users.");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error occurred while getting all users");
+            throw;
+        }
     }
 
     public async Task<Result<string>> DeleteUserAsync(string userName)
     {
-        throw new NotImplementedException();
+        try
+        {
+            _logger.LogInformation("Deleting user: {userName}", userName);
+            throw new NotImplementedException();
+            // _logger.LogInformation("Successfully deleted user: {userName}", userName);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error occurred while deleting user: {userName}", userName);
+            throw;
+        }
     }
 }
