299eca42a40d6a9b713e896f541532f44c16c8f1
Anton Zhilin <anton@innovine.nl>
2023-07-29 16:38:22 +0200

    Add unit tests for UserRandomizer.

---
+++ Cnvs.Demo.TaskManagement.Tests/When_getting_random_user.cs
@@ -0,0 +1,35 @@
+﻿using Cnvs.Demo.TaskManagement.Domain;
+using FakeItEasy;
+using FluentAssertions;
+
+namespace Cnvs.Demo.TaskManagement.Tests;
+
+public class When_getting_random_user
+{
+    private readonly UserRandomizer _userRandomizer;
+
+    public When_getting_random_user()
+    {
+        _userRandomizer = new UserRandomizer();
+    }
+
+    [Fact]
+    public void Should_Return_RandomUser_When_Users_Available()
+    {
+        var users = A.CollectionOfDummy<User>(5);
+
+        var user = _userRandomizer.GetRandomUser(users);
+
+        user.Should().NotBe(NullUser.Instance);
+    }
+
+    [Fact]
+    public void Should_Return_NullUser_When_No_Users_Available()
+    {
+        IEnumerable<User> users = new List<User>();
+
+        var user = _userRandomizer.GetRandomUser(users);
+
+        user.Should().Be(NullUser.Instance);
+    }
+}
