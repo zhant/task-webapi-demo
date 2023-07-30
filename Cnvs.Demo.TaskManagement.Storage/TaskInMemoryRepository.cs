36da468e7ef35b7d3785b6939d02590f3d54ab5c
Anton Zhilin <anton@innovine.nl>
2023-07-30 18:34:11 +0200

    Add InMemory repositories for User and Task
    This commit introduces new classes, UserInMemoryRepository and TaskInMemoryRepository, within Cnvs.Demo.TaskManagement.Storage. These classes simulate an in-memory data store for both User and Task entities from the TaskManagement.Domain.

---
+++ Cnvs.Demo.TaskManagement.Storage/TaskInMemoryRepository.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Immutable;
+using Cnvs.Demo.TaskManagement.Domain;
+using DomainTask = Cnvs.Demo.TaskManagement.Domain.Task;
+
+namespace Cnvs.Demo.TaskManagement.Storage;
+
+public class TaskInMemoryRepository : ITaskRepository
+{
+    private readonly ConcurrentDictionary<Guid, DomainTask> _tasks = new();
+
+    public async Task<Result<string>> DeleteTaskAsync(Guid taskId)
+    {
+        var success = _tasks.TryRemove(taskId, out var _);
+        
+        return success 
+            ? Result<string>.Success(taskId.ToString())
+            : Result<string>.Failure("Task not found", taskId.ToString());
+    }
+
+    public async Task<Result<DomainTask>> AddTask(DomainTask task)
+    {
+        _tasks[task.Id] = task;
+ 
+        return Result<DomainTask>.Success(task);
+    }
+
+    public async Task<Result<DomainTask>> GetTaskAsync(Guid taskId)
+    {
+        var taskExists = _tasks.TryGetValue(taskId, out var task);
+        
+        return taskExists 
+            ? Result<DomainTask>.Success(task!)
+            : Result<DomainTask>.Failure("Task not found", NullTask.Instance);
+    }
+
+    public async Task<Result<IEnumerable<DomainTask>>> GetTasksAsync(TaskState[] taskStates)
+    {
+        var tasks = _tasks.Values.Where(t => taskStates.Contains(t.State)).ToList();
+        
+        return Result<IEnumerable<DomainTask>>.Success(tasks);
+    }
+
+    public async Task<Result<IEnumerable<DomainTask>>> GetTasksAsync(TaskState taskState)
+    {
+        var tasks = _tasks.Values.Where(t => t.State == taskState).ToList();
+
+        return Result<IEnumerable<DomainTask>>.Success(tasks);
+    }
+
+    public async Task<Result<DomainTask>> UpdateTaskAsync(DomainTask task)
+    {
+        _tasks[task.Id] = task;
+        
+        return Result<DomainTask>.Success(task);
+    }
+
+    public async Task<Result<IEnumerable<DomainTask>>> GetUserTasksAsync(string userName)
+    {
+        var tasks = _tasks.Values.Where(t => t.AssignedUser?.Name == userName).ToList();
+
+        return tasks.Any() 
+            ? Result<IEnumerable<DomainTask>>.Success(tasks) 
+            : Result<IEnumerable<DomainTask>>.Failure("No tasks found for user", ImmutableList<DomainTask>.Empty);
+    }
+
+    public Result<IEnumerable<DomainTask>> GetTasks()
+    {
+        var tasks = _tasks.Values.ToList();
+
+        return tasks.Any() 
+            ? Result<IEnumerable<DomainTask>>.Success(tasks) 
+            : Result<IEnumerable<DomainTask>>.Failure("No tasks found", ImmutableList<DomainTask>.Empty);
+    }
+}
---
+++ Cnvs.Demo.TaskManagement.Storage/UserInMemoryRepository.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Immutable;
+using Cnvs.Demo.TaskManagement.Domain;
+using DomainTask = Cnvs.Demo.TaskManagement.Domain.Task;
+
+namespace Cnvs.Demo.TaskManagement.Storage;
+
+public class UserInMemoryRepository : IUserRepository
+{
+    private readonly ConcurrentDictionary<string, User> _users = new();
+
+    public async Task<Result<User>> AddUser(User user)
+    {
+        var added = _users.TryAdd(user.Name, user);
+        return !added 
+            ? Result<User>.Failure("User already exists", NullUser.Instance) 
+            : Result<User>.Success(user);
+    }
+
+    public async Task<Result<User>> GetUserAsync(string userName)
+    {
+        _users.TryGetValue(userName, out var user);
+        user ??= NullUser.Instance;
+
+        return user is NullUser 
+            ? Result<User>.Failure("User not found", user) 
+            : Result<User>.Success(user);
+    }
+
+    public Result<IEnumerable<User>> GetUsers()
+    {
+        var users = _users.Values.ToList();
+
+        return users.Any() 
+            ? Result<IEnumerable<User>>.Success(users) 
+            : Result<IEnumerable<User>>.Failure("No users found", ImmutableList<User>.Empty);
+    }
+
+    public async Task<Result<string>> DeleteUserAsync(string userName)
+    {
+        var removed = _users.TryRemove(userName, out var _);
+        
+        return removed 
+            ? Result<string>.Success(userName) 
+            : Result<string>.Failure("User not found", userName);
+    }
+}
