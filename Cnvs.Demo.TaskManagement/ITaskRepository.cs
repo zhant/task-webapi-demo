d277233e258074dcab85ec741f845ebfb86b43ed
Anton Zhilin <anton@innovine.nl>
2023-07-30 18:33:28 +0200

    Refactor ITaskRepository for consistency.For clarity and consistency, all instances of 'Task' have been changed to 'DomainTask'.

--- Cnvs.Demo.TaskManagement/ITaskRepository.cs
+++ Cnvs.Demo.TaskManagement/ITaskRepository.cs
@@ -7,10 +7,10 @@ public interface ITaskRepository
 {
     Task<Result<string>> DeleteTaskAsync(Guid taskId);  
     Task<Result<DomainTask>> AddTask(Domain.Task task);
-    Task<Result<Domain.Task>> GetTaskAsync(Guid taskId);
+    Task<Result<DomainTask>> GetTaskAsync(Guid taskId);
     Task<Result<IEnumerable<DomainTask>>> GetTasksAsync(TaskState[] taskStates);
     Task<Result<IEnumerable<DomainTask>>> GetTasksAsync(TaskState taskState);
-    Task<Result<Domain.Task>> UpdateTaskAsync(Domain.Task task);
+    Task<Result<DomainTask>> UpdateTaskAsync(Domain.Task task);
     Task<Result<IEnumerable<Domain.Task>>> GetUserTasksAsync(string userName);
-    Result<IEnumerable<Domain.Task>> GetTasks();
+    Result<IEnumerable<DomainTask>> GetTasks();
 }
