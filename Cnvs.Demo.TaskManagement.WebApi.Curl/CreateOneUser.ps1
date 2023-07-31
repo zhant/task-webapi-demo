1abc8f0728b83010eaf4426f7982805e451c7619
Anton Zhilin <anton@innovine.nl>
2023-07-31 23:13:46 +0200

    Add scripts for user creation in TaskManagement API
    Two PowerShell scripts are added in 'Cnvs.Demo.TaskManagement.WebApi.Curl' for creating users in the TaskManagement API. CreateOneUser.ps1 creates a single user, while CreateThreeUsers.ps1 creates three users. The scripts use 'Invoke-RestMethod' cmdlet for making POST requests to the API with user details in the body. This will automate and ease the process of creating users for

---
+++ Cnvs.Demo.TaskManagement.WebApi.Curl/CreateOneUser.ps1
@@ -0,0 +1,9 @@
+﻿$userToCreateJson = @{
+    "Name" = "John Doe"
+} | ConvertTo-Json
+
+Invoke-RestMethod `
+-Uri "http://localhost:5188/task-management/" `
+-Method Post `
+-Body $userToCreateJson `
+-ContentType "application/json"
---
+++ Cnvs.Demo.TaskManagement.WebApi.Curl/CreateThreeUsers.ps1
@@ -0,0 +1,13 @@
+﻿$users = @("John Doe Minor", "Jane Doe", "Jim Doe")
+
+$users | ForEach-Object {
+    $userToCreateJson = @{
+        "Name" = $_
+    } | ConvertTo-Json
+
+    Invoke-RestMethod `
+    -Uri "http://localhost:5188/task-management/" `
+    -Method Post `
+    -Body $userToCreateJson `
+    -ContentType "application/json"
+}
