2cd1ec565670d12b73df50af85ceaafde48f3912
Anton Zhilin <anton@innovine.nl>
2023-07-31 21:11:40 +0200

    Remove stub methods from ITaskEngine.

--- Cnvs.Demo.TaskManagement/ITaskEngine.cs
+++ Cnvs.Demo.TaskManagement/ITaskEngine.cs
@@ -6,22 +6,14 @@ namespace Cnvs.Demo.TaskManagement;
 public interface ITaskEngine
 {
     Task<Result<IEnumerable<Domain.Task>>> GetTasksAsync(TaskState state = TaskState.Undefined);
-
     Task<Result<Domain.Task>> GetTaskAsync(Guid id);
-    
     Task<Result<Domain.Task>> CreateTaskAsync(string taskDescription);
     Task<Result<string>> DeleteTaskAsync(Guid id);
-    
-    Task<Result<IEnumerable<User>>> GetUsersAsync() => 
-        Task.FromResult(Result<IEnumerable<User>>.Success(Enumerable.Empty<User>()));
-
+    Task<Result<IEnumerable<User>>> GetUsersAsync();
     Task<Result<User>> GetUserByNameAsync(string name);
-    Task<Result<User>> CreateUserAsync(User user) => Task.FromResult(Result<User>.Success(user));
-
-    Task<Result<string>> DeleteUserAsync(string name) => Task.FromResult(Result<string>.Success(name));
-    
+    Task<Result<User>> CreateUserAsync(User user);
+    Task<Result<string>> DeleteUserAsync(string name);
     Task<Result<IEnumerable<Domain.Task>>> GetUserTasksByUserNameAsync(string name);
-
     void RotateTask(Domain.Task task);
     Task<Result<IEnumerable<Domain.Task>>> GetTasksAsync(TaskState[] allowedStates);
     Task<Result<IEnumerable<Domain.Task>>> GetTasks(TaskState[] allowedStates);
