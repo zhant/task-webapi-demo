906c17330efc2f6eb74ab2164b7066f1edd7fdbb
Anton Zhilin <anton@innovine.nl>
2023-07-31 21:13:33 +0200

    Reformat code.

--- Cnvs.Demo.TaskManagement/ITaskRepository.cs
+++ Cnvs.Demo.TaskManagement/ITaskRepository.cs
@@ -5,7 +5,6 @@ namespace Cnvs.Demo.TaskManagement;
 
 public interface ITaskRepository
 {
-    Task<Result<string>> DeleteTaskAsync(Guid taskId);  
     Task<Result<DomainTask>> AddTask(Domain.Task task);
     Task<Result<DomainTask>> GetTaskAsync(Guid taskId);
     Task<Result<IEnumerable<DomainTask>>> GetTasksAsync();
@@ -16,4 +15,5 @@ public interface ITaskRepository
     Result<IEnumerable<DomainTask>> GetTasks();
     Task<Result<IEnumerable<DomainTask>>> GetUserTasksAsync(Guid id);
     Task<Result<IEnumerable<User>>> GetTaskUsersAsync(Guid id);
+    Task<Result<string>> DeleteTaskAsync(Guid taskId);
 }
