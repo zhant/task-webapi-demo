73f1e5f3a99ff7c76f6c28e9a84ea4345cd48acf
Anton Zhilin <anton@innovine.nl>
2023-07-31 23:07:15 +0200

    Update baseUrl in CreateXTasks script
    Corrected the baseUrl variable in the CreateXTasks.ps1 script. The previous url contained an extraneous segment ('taskToCreate-'), which has now been removed to accurately point to 'http://localhost:5188/task-management/'. This change will ensure that tasks are properly created through the API endpoint.

--- Cnvs.Demo.TaskManagement.WebApi.Curl/CreateXTasks.ps1
+++ Cnvs.Demo.TaskManagement.WebApi.Curl/CreateXTasks.ps1
@@ -1,4 +1,4 @@
-﻿Set-Variable -Name 'baseUrl' -Value 'http://localhost:5188/taskToCreate-management/' -Option Constant
+﻿Set-Variable -Name 'baseUrl' -Value 'http://localhost:5188/task-management/' -Option Constant
 Set-Variable -Name 'numTasks' -Value 10
 $apiEndpoint = $baseUrl + 'tasks'
 for ($i=1; $i -le $numTasks; $i++) {
