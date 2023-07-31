e426419ae403110fd06c93bc7b7d5a5d17ecdf87
Anton Zhilin <anton@innovine.nl>
2023-07-31 23:36:20 +0200

    Add unit tests for UsersController and TasksController.

---
+++ Cnvs.Demo.TaskManagement.WebApi.Tests/ControllerTests/When_calling_tasks_controller.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using Cnvs.Demo.TaskManagement.Domain;
+using Cnvs.Demo.TaskManagement.Dto;
+using Cnvs.Demo.TaskManagement.WebApi.Controllers;
+using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Task = System.Threading.Tasks.Task;
+
+namespace Cnvs.Demo.TaskManagement.WebApi.Tests.ControllerTests;
+
+public class When_calling_tasks_controller
+{
+    private readonly ITaskEngine _fakeTaskEngine;
+    private readonly IMapper _fakeMapper;
+    private readonly TasksController _controller;
+    
+    public When_calling_tasks_controller()
+    {
+        _fakeTaskEngine = A.Fake<ITaskEngine>();
+        _fakeMapper = A.Fake<IMapper>();
+        _controller = new TasksController(_fakeTaskEngine, _fakeMapper);
+    }
+    
+    [Fact]
+    public async Task CreateTask_Succeeds()
+    {
+        // Arrange
+        var taskToCreate = new TaskToCreate { Description = "Test task" };
+
+        var domainTask = Domain.Task.NewTask(taskToCreate.Description);
+        A.CallTo(() => _fakeTaskEngine.CreateTaskAsync(taskToCreate.Description))
+            .Returns(Task.FromResult(Result<Domain.Task>.Success(domainTask)));
+
+        var dtoTask = new Dto.Task { Id = domainTask.Id, Description = domainTask.Description };
+        A.CallTo(() => _fakeMapper.Map<Dto.Task>(A<Domain.Task>._)).Returns(dtoTask);
+
+        // Act
+        var result = await _controller.CreateTask(taskToCreate);
+
+        // Assert
+        result.Should().BeOfType<CreatedAtActionResult>();
+        var createdAtActionResult = (CreatedAtActionResult)result;
+        createdAtActionResult.Value.Should().BeOfType<Dto.Task>().Which.Description.Should().Be(taskToCreate.Description);
+    }
+
+    [Fact]
+    public async Task CreateTask_Fails()
+    {
+        // Arrange
+        var taskToCreate = new TaskToCreate { Description = "Test task" };
+        A.CallTo(() => _fakeTaskEngine.CreateTaskAsync(taskToCreate.Description))
+            .Returns(Task.FromResult(Result<Domain.Task>.Failure("Error message", NullTask.Instance)));
+
+        // Act
+        var result = await _controller.CreateTask(taskToCreate);
+
+        // Assert
+        result.Should().BeOfType<BadRequestObjectResult>();
+        var badRequestResult = (BadRequestObjectResult)result;
+        badRequestResult.Value.Should().Be("Error message");
+    }   
+}
---
+++ Cnvs.Demo.TaskManagement.WebApi.Tests/ControllerTests/When_calling_users_controller.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using Cnvs.Demo.TaskManagement.Domain;
+using Cnvs.Demo.TaskManagement.Dto;
+using Cnvs.Demo.TaskManagement.WebApi.Controllers;
+using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Task = System.Threading.Tasks.Task;
+using User = Cnvs.Demo.TaskManagement.Dto.User;
+
+namespace Cnvs.Demo.TaskManagement.WebApi.Tests.ControllerTests;
+
+
+public class When_calling_users_controller
+{
+    private readonly ITaskEngine _fakeTaskEngine;
+    private readonly IMapper _fakeMapper;
+    private readonly UsersController _controller;
+
+    public When_calling_users_controller()
+    {
+        _fakeTaskEngine = A.Fake<ITaskEngine>();
+        _fakeMapper = A.Fake<IMapper>();
+        _controller = new UsersController(_fakeMapper, _fakeTaskEngine);
+    }
+
+    [Fact]
+    public async Task AddUser_Succeeds()
+    {
+        // Arrange
+        var userDto = new UserToCreate { Name = "Test User" };
+        var domainUser = Domain.User.Create(userDto.Name);
+        domainUser.CreatedAt = DateTime.UtcNow;
+        A.CallTo(() => _fakeTaskEngine.CreateUserAsync(A<Domain.User>._))
+            .Returns(Task.FromResult(Result<Domain.User>.Success(domainUser)));
+
+        var dtoUser = new User { Id = domainUser.Id, Name = domainUser.Name };
+        A.CallTo(() => _fakeMapper.Map<User>(A<Domain.User>._)).Returns(dtoUser);
+
+        // Act
+        var result = await _controller.AddUser(userDto);
+
+        // Assert
+        result.Should().BeOfType<CreatedAtActionResult>();
+        var createdAtActionResult = (CreatedAtActionResult)result;
+        createdAtActionResult.Value.Should().BeOfType<User>().Which.Name.Should().Be(userDto.Name);
+    }
+
+    [Fact]
+    public async Task AddUser_Fails()
+    {
+        // Arrange
+        var userDto = new UserToCreate { Name = "Test User" };
+        A.CallTo(() => _fakeTaskEngine.CreateUserAsync(A<Domain.User>._))
+            .Returns(Task.FromResult(Result<Domain.User>.Failure("Error message", NullUser.Instance)));
+
+        // Act
+        var result = await _controller.AddUser(userDto);
+
+        // Assert
+        result.Should().BeOfType<BadRequestObjectResult>();
+        var badRequestResult = (BadRequestObjectResult)result;
+        badRequestResult.Value.Should().Be("Error message");
+    }
+}
