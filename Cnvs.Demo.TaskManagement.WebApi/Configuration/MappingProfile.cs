ea290f7210a631d8f7716a0a34c8805330e3706c
Anton Zhilin <anton@innovine.nl>
2023-08-01 12:29:31 +0200

    Refactor mapping code to handle null user instances. The refactoring consolidates the handling into a single mapping line which explicitly constructs a NullUser instance as a null of Dto.User.

--- Cnvs.Demo.TaskManagement.WebApi/Configuration/MappingProfile.cs
+++ Cnvs.Demo.TaskManagement.WebApi/Configuration/MappingProfile.cs
@@ -13,17 +13,15 @@ public class MappingProfile : AutoMapper.Profile
         CreateMap<Cnvs.Demo.TaskManagement.Dto.Task, Task>()
             .ForMember(dest => dest.State, opt => opt.MapFrom(src => (TaskState)src.State))
             .ForMember(dest => dest.AssignedUser, opt => opt.MapFrom(src => src.AssignedUser))
-            .ForMember(dest => dest.AssignedUser, opt => opt.NullSubstitute(NullUser.Instance))
             .ForMember(dest => dest.AssignedUsersHistory, opt => opt.MapFrom(src => src.AssignedUsersHistory));
+
+        
+        CreateMap<NullUser, Cnvs.Demo.TaskManagement.Dto.User>().ConstructUsing(src => (null as Cnvs.Demo.TaskManagement.Dto.User)!);
 
         CreateMap<Task, Cnvs.Demo.TaskManagement.Dto.Task>()
             .ForMember(dest => dest.State, opt => opt.MapFrom(src => (Cnvs.Demo.TaskManagement.Dto.TaskState)src.State))
-            .ForMember(dest => dest.AssignedUser, opt => opt.MapFrom(src => src.AssignedUser == NullUser.Instance ? null : src.AssignedUser))
-            .AfterMap((src, dest) =>
-            {
-                dest.AssignedUsersHistory = dest.AssignedUsersHistory
-                    .Where(user => user.GetType() != typeof(NullUser))
-                    .ToArray();
-            });
+            .ForMember(dest => dest.AssignedUser, opt => opt.MapFrom(src => src.AssignedUser))
+            .ForMember(dest => dest.AssignedUsersHistory, opt => opt.MapFrom(src => src.AssignedUsersHistory))
+            .ForMember(dest => dest.AssignedUser, opt => opt.MapFrom(src => src.AssignedUser is NullUser ? null : src.AssignedUser));
     }
 }
