e4e80ce56800846d476fea03302b8cd5b44b0143
Anton Zhilin <anton.zhilin@gmail.com>
2023-08-01 18:05:24 +0200

    Enable Swagger in all environments.

--- Cnvs.Demo.TaskManagement.WebApi/Program.cs
+++ Cnvs.Demo.TaskManagement.WebApi/Program.cs
@@ -38,11 +38,8 @@ builder.Services.AddHostedService<TaskRotationService>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
